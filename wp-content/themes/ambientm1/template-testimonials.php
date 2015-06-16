26e425710a5e77a2f673a64fa06ec8be7dd3f926
edgemm-gavin <gavin@edgemm.com>
2015-06-16 11:36:36 -0700

    Update Testimonials template

--- wp-content/themes/ambientm1/template-testimonials.php
+++ wp-content/themes/ambientm1/template-testimonials.php
@@ -2,17 +2,7 @@
 	
 	<!-- section -->
 	<div role="main" id="main">
-	
-		<div class="hero">
-			<div class="container">
-				<div class="span10 offset1">
-					<h2><?php the_title(); ?></h2>
-					<h3><?php the_field( 'subtitle' ); ?></h3>
-					<hr />
-					<p><?php the_field( 'intro_copy' ); ?></p>
-				</div>
-			</div>
-	    </div>
+
 		<div class="content-panel" id="testimonials">
 		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
@@ -36,27 +26,4 @@
 		<?php endif; ?>
 
 		</div>
-		<div class="dr-bios" id="meet-our-doctors">
-			<div class="container">
-				<h4><?php the_field( 'doctor_bio_header' ); ?></h4>
-				<?php get_template_part('loop-drs'); ?>
-			</div>
-		</div>
-		<div class="team-bios" id="meet-the-crew">
-			<div class="container">
-				<h4><?php the_field( 'team_bio_header' ); ?></h4>
-				<?php get_template_part('loop-team'); ?>
-			</div>
-		</div>
-		<div class="meet-flosser" id="meet-flosser">
-			<div class="container">
-				<span>Flosser</span>
-				<div class="row">
-					<div class="span7 offset1">
-						<h4><?php the_field( 'flosser_header' ); ?></h4>
-						<p><?php the_field( 'flosser_copy' ); ?></p>
-					</div>
-				</div>
-			</div>
-		</div>	
 	<?php get_footer(); ?>
